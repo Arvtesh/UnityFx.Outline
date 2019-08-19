f37a520d102aa56dd6f510bcb883976bb7d38de1
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2019-08-19 17:53:15 +0300

    Made OutlineRenderer public and added comments

--- Assets/Plugins/UnityFx.Outline/Runtime/Scripts/OutlineRenderer.cs
+++ Assets/Plugins/UnityFx.Outline/Runtime/Scripts/OutlineRenderer.cs
@@ -7,7 +7,22 @@ using UnityEngine.Rendering;
 
 namespace UnityFx.Outline
 {
-	internal struct OutlineRenderer : IDisposable
+	/// <summary>
+	/// Helper low-level class for building outline <see cref="CommandBuffer"/>.
+	/// </summary>
+	/// <remarks>
+	/// This class is used by higher level outline implementations (<see cref="OutlineEffect"/> and <see cref="OutlineBehaviour"/>).
+	/// It implements <see cref="IDisposable"/> to be used with C# inside <see langword="using"/> block as shown in the code sample.
+	/// </remarks>
+	/// <example>
+	/// using (var renderer = new OutlineRenderer(commandBuffer, BuiltinRenderTextureType.CameraTarget))
+	/// {
+	/// 	renderer.RenderSingleObject(outlineRenderers, renderMaterial, postProcessMaterial);
+	/// }
+	/// </example>
+	/// <seealso cref="OutlineEffect"/>
+	/// <seealso cref="OutlineBehaviour"/>
+	public struct OutlineRenderer : IDisposable
 	{
 		#region data
 
@@ -19,20 +34,47 @@ namespace UnityFx.Outline
 
 		#region interface
 
+		/// <summary>
+		/// A <see cref="CameraEvent"/> outline rendering should be assosiated with.
+		/// </summary>
 		public const CameraEvent RenderEvent = CameraEvent.BeforeImageEffects;
 
+		/// <summary>
+		/// Name of the outline effect.
+		/// </summary>
 		public const string EffectName = "Outline";
+
+		/// <summary>
+		/// Name of the outline color shader parameter.
+		/// </summary>
 		public const string ColorParamName = "_Color";
+
+		/// <summary>
+		/// Name of the outline width shader parameter.
+		/// </summary>
 		public const string WidthParamName = "_Width";
 
+		/// <summary>
+		/// Minimum value of outline width parameter.
+		/// </summary>
 		public const int MinWidth = 1;
+
+		/// <summary>
+		/// Maximum value of outline width parameter.
+		/// </summary>
 		public const int MaxWidth = 32;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OutlineRenderer"/> struct.
+		/// </summary>
 		public OutlineRenderer(CommandBuffer commandBuffer, BuiltinRenderTextureType dst)
 			: this(commandBuffer, new RenderTargetIdentifier(dst))
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OutlineRenderer"/> struct.
+		/// </summary>
 		public OutlineRenderer(CommandBuffer commandBuffer, RenderTargetIdentifier dst)
 		{
 			Debug.Assert(commandBuffer != null);
@@ -46,6 +88,9 @@ namespace UnityFx.Outline
 			_commandBuffer.GetTemporaryRT(_renderTextureId, -1, -1, 0, FilterMode.Bilinear, RenderTextureFormat.R8);
 		}
 
+		/// <summary>
+		/// Adds commands for rendering single outline object.
+		/// </summary>
 		public void RenderSingleObject(Renderer[] renderers, Material renderMaterial, Material postProcessMaterial)
 		{
 			Debug.Assert(renderers != null);
@@ -75,6 +120,7 @@ namespace UnityFx.Outline
 
 		#region IDisposable
 
+		/// <inheritdoc/>
 		public void Dispose()
 		{
 			_commandBuffer.ReleaseTemporaryRT(_renderTextureId);
