23abd2e022813c506c712056a0fb687c51ca908f
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2019-09-26 18:21:45 +0300

    Assembly info fixes

--- Assets/Plugins/UnityFx.Outline/Runtime/Scripts/Helpers/AssemblyInfo.cs
+++ Assets/Plugins/UnityFx.Outline/Runtime/Scripts/Helpers/AssemblyInfo.cs
@@ -5,24 +5,27 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UnityFx.Outline")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnityFx.Outline")]
+[assembly: AssemblyDescription("Screen-space outlines for Unity3d.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright © Alexander Bogarsukov 2019")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
 [assembly: Guid("1ace8625-97c5-4d37-a649-03975d187542")]
 
 // Make internals visible to the editor assembly.
