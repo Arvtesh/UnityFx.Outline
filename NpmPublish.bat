24bf7b73415aba9562943f4dc348f36edde0b1db
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2020-04-02 15:42:47 +0300

    Updated publish scripts

--- NpmPublish.bat
+++ NpmPublish.bat
@@ -1,2 +1,3 @@
-npm publish Packages\UnityFx.Outline
-npm publish Packages\UnityFx.Outline.PostProcessing
+npm publish Outline.Core\Packages\UnityFx.Outline
+npm publish Outline.PostProcessing\Packages\UnityFx.Outline.PostProcessing
+npm publish Outline.HDRP\Packages\UnityFx.Outline.HDRP
