7614db0963cda135e6a0093bcc65eeff74776986
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2021-02-15 22:22:17 +0200

    Copyright update

--- Outline.Core/Packages/UnityFx.Outline/Runtime/Scripts/Properties/AssemblyInfo.cs
+++ Outline.Core/Packages/UnityFx.Outline/Runtime/Scripts/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2019-2020 Alexander Bogarsukov. All rights reserved.
+﻿// Copyright (C) 2019-2021 Alexander Bogarsukov. All rights reserved.
 // See the LICENSE.md file in the project root for more information.
 
 using System.Reflection;
